46ecf3050b88a1c4a468f9aed937896dceded40e
amit1509kumar <amit1509kumar@gmail.com>
2020-07-25 23:10:12 +0200

    First problem in CSES problem set

---
+++ src/IntroductoryProblems/WeirdAlgorithm.java
@@ -0,0 +1,27 @@
+package IntroductoryProblems;
+
+import java.util.Scanner;
+
+public class WeirdAlgorithm
+{
+    public static void main(String[] args) {
+           Solve();
+    }
+
+    public static void Solve()
+    {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextInt();
+        System.out.print(n);
+        while(n > 1)
+        {
+            if(n%2 == 0)
+                n /=2;
+            else
+                n = n * 3 + 1;
+
+            System.out.print(" " + n);
+        }
+
+    }
+}
